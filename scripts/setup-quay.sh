d93e89eab195038eff4073f1c2b82529595b2310
Erick Quiñonez <erick.eliseo@gmail.com>
2020-06-23 15:22:04 -0600

    Update setup-quay.sh

--- scripts/setup-quay.sh
+++ scripts/setup-quay.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 cd ~/
-git clone https://github.com/erickeliseo/k8sfiles.git
-sleep 30
 mkdir -p /mnt/quay/config
 mkdir -p /mnt/quay/storage
-
 cp ~/k8sfiles/quay/quay-config.tar.gz /mnt/quay/config/
 cp ~/k8sfiles/quay/certs/server.crt /etc/pki/ca-trust/source/anchors/
 cp ~/k8sfiles/quay/certs/rootCA.crt /etc/pki/ca-trust/source/anchors/
-
 cd /mnt/quay/config/
 tar xvf quay-config.tar.gz
-sleep 5
+sleep 3
 chmod -R 777 /mnt/quay/storage/
 update-ca-trust
-sleep 5
-systemctl restart docker
+systemctl restart docker ; systemctl status docker
